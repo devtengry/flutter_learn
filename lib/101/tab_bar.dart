a2f3847bad6b6ed5969e4797b1c4df0c4ac8a3bb
devtengry <imamogluomer@outlook.com>
2025-01-23 23:58:04 +0300

    TabBarLearn class integrated.

---
+++ lib/101/tab_bar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TabBarLearn extends StatefulWidget {
+  const TabBarLearn({super.key});
+
+  @override
+  State<TabBarLearn> createState() => _TabBarLearnState();
+}
+
+class _TabBarLearnState extends State<TabBarLearn> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
